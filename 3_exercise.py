f36c222f03385b4078691653384bebb4d53bb12b
euriel-calimlim <eurielv4@gmail.com>
2025-10-26 05:21:22 +0400

    Update 3_exercise.py

--- 3_exercise.py
+++ 3_exercise.py
@@ -4,7 +4,8 @@ Exercise 3: Biography
 name = input("Enter your name: ")
 hometown = input("Enter your hometown: ")
 age = int(input("Enter your age: "))
-#asks for user to input their name, hometown and age. It allows any data type like integer,string etc;to be entered.
+#asks for user to input their name, hometown and age.
+#age variable uses 'int(input' which only allows users to input integer data type.
 biography={"name=":name,"hometown=":hometown,"age=":age} #Create a dictionary called 'biography' to store the user's information
 # The questions are strings like "name=",etc.
 # The answers are the user inputs stored in the variables above
@@ -13,4 +14,5 @@ for question,answer in biography.items(): #Using a For-loop  to loop through eac
 
     print("\n",question,answer) #prints on a new line;
     #the key(e.g. "name="), then the corresponding value that the user inputs for that question(e.g. jonathan).
+
 
